5906ef8c9083ec49dc1906c65898ddb1400d7dd8
rain-35 <113638519+rain-35@users.noreply.github.com>
2026-04-24 10:37:13 -0600

    prep 1 & 2 prove 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is you grade percentage? ");
+        float grade_num = float.Parse(Console.ReadLine());
+
+        string grade_let = "error";
+
+
+        if (grade_num >= 90)
+        {
+            grade_let = "A";
+        }
+        else if (grade_num >= 80)
+        {
+            grade_let = "B";
+        }
+        else if (grade_num >= 70)
+        {
+            grade_let = "C";
+        }
+        else if (grade_num >= 60)
+        {
+            grade_let = "D";
+        }
+        else
+        {
+            grade_let = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade_let}.");
+
+        if (grade_num >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed.");
+        }
+        
     }
 }
