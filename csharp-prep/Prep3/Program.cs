705a48b6f78f314ef41bcb551a7c6a27acc72db4
rain-35 <113638519+rain-35@users.noreply.github.com>
2026-04-30 12:46:43 -0600

    ff

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response = "yes";
+        int low = 0;
+        int high = 10;
+        int guesses = 0;
+
+        while (response == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(low, high + 1);
+            // Console.WriteLine(number);   
+            
+            while (true)
+            {
+                Console.Write("What is your guess? ");
+                string guess = Console.ReadLine();
+                int guess_num = int.Parse(guess);
+                guesses++;
+                if (guess_num == number)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine("You took " + guesses + " guesses.");
+                    break;
+                }else if (guess_num > number)
+                {
+                    Console.WriteLine("Lower");
+                }else if (guess_num < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                
+
+            }
+
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+
+        }
+
+
+
+
+
     }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        while (true);
+       
+        Console.WriteLine("didnt work");
     }
 }
