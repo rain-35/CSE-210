9878f07b3cfd01daaddda5fe9905fd59269438cb
rain-35 <113638519+rain-35@users.noreply.github.com>
2026-05-01 12:08:58 -0600

    prep week 2

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int input_num = 1;
+        List<int> numbers;
+        numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers one at a time, type 0 when finished.");
+
+        while (input_num != 0)
+        {
+            
+            Console.Write("Enter a number: ");
+            input_num = int.Parse(Console.ReadLine());
+            if (input_num != 0)
+            {
+                numbers.Add(input_num);
+            }
+        }
+        Console.WriteLine("The list is: " + string.Join(", ", numbers));
+
+        
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = GetUserName();
+        int number = GetUserNumber();
+        int birthYear = GetBirthYear();
+        int square = SquareNumber(number);
+        DisplayUserInfo(name, square, birthYear);
+    }
+    
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string GetUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int GetUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int GetBirthYear()
+    {
+        Console.WriteLine("Please enter your birth year: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayUserInfo(string name, int square, int birthYear)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+        Console.WriteLine($"You will turn {2026 - birthYear} this year.");
     }
 }
