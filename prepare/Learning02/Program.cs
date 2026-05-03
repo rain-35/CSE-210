a221ef09808d910350c63c96cc8ab09665d2e92c
rain-35 <113638519+rain-35@users.noreply.github.com>
2026-05-03 16:08:00 -0600

    learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,9 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-        while (true);
-       
-        Console.WriteLine("didnt work");
+        Job job1 = new Job();
+        job1._jobTitile = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "2019";
+        job1._endYear = "2022";
+
+        Job job2 = new Job();
+        job2._jobTitile = "Engineer";
+        job2._company = "Proctor and Gamble";
+        job2._startYear = "2022";
+        job2._endYear = "2024";
+
+        Resume resume1 = new Resume();
+        resume1._name = "Rainen Morriss";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
