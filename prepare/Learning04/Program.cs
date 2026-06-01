0159dac644bbd401a6a3df5982daad6523fb40a9
rain-35 <113638519+rain-35@users.noreply.github.com>
2026-05-31 18:23:05 -0600

    Learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("John", "C#");
+
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("John", "Math", "Big Math", "1, 2, 3, 4, 5");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment writing1 = new WritingAssignment("John", "Writing", "Big Writing");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
     }
 }
